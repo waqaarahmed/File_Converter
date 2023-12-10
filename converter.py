bf749af129c2af9b1a1e5b9f99cf36c4d0c2675c
waqaarahmed <ahmedwaqar309@gmail.com>
2023-12-10 23:45:30 +0500

    new files

--- converter.py
+++ converter.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 #from json to csv
 to_csv = pd.read_json(r"C:\Users\ahmed\.vscode\File_Converter\intents.json")
@@ -7,3 +8,10 @@ to_csv.to_csv(r"C:\Users\ahmed\.vscode\File_Converter\intents.csv")
 #from csv to json
 from_csv = pd.read_csv(r"C:\Users\ahmed\.vscode\File_Converter\intents.csv")
 from_csv.to_json(r"C:\Users\ahmed\.vscode\File_Converter\converted_from_csv.json")
+
+#from json to text file
+file = open('intents.json')
+data = json.load(file)
+js_file = json.dumps(data)
+with open("intents.txt", "w") as f:
+    f.write(js_file)
