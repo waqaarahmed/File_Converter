7d2fd22cd31d94a34ab18f3a8185f289e48ba3de
waqaarahmed <ahmedwaqar309@gmail.com>
2023-12-12 15:06:06 +0500

    new files

--- converter.py
+++ converter.py
@@ -15,3 +15,17 @@ data = json.load(file)
 js_file = json.dumps(data)
 with open("intents.txt", "w") as f:
     f.write(js_file)
+    
+#from text to json
+file = 'intents.txt'
+dict1 = {}
+with open(file) as fh:
+ 
+    for line in fh:
+        command, description = line.strip().split(None, 1)
+        dict1[command] = description.strip()
+
+out_file = open("test1.json", "w")
+json.dump(dict1, out_file, indent = 4, sort_keys = False)
+out_file.close()
+        
